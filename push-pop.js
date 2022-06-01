efa08f52432f293b90787ec4dfc5238d6e8f7299
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-06-01 10:21:51 +0600

    first commit

---
+++ push-pop.js
@@ -0,0 +1,39 @@
+// // notun kore kau ke add korte caile push kora jay ...tobe seta string er sesh e bosbe ...
+// // add elements in a array 
+
+
+// var lastBantch = ['kalajm', 'balam', 'salam'];
+// lastBantch.push('hakim');
+// lastBantch.push('juakim');
+// lastBantch.push('hhakim');
+// lastBantch.push('hbbcxakim');
+// console.log(lastBantch);
+
+
+
+// var frIendage = [10, 12, 3, 80, 11, 5];
+// frIendage.push(14);
+// console.log(frIendage);
+
+
+
+
+// // remove elemenet a array 
+// var frIendageremove = [10, 12, 3, 80, 11, 5];
+// frIendageremove.pop();
+// frIendageremove.pop();
+// frIendageremove.pop();
+// console.log(frIendageremove);
+
+
+
+
+// jei item ta  remove pop korbo seita sohokar e rakhara jonno amra eivabe code likbo 
+var Shoping = [4, 2, 36, 9, 7];
+var lastItem = Shoping.pop();
+console.log(Shoping);
+console.log(lastItem);
+
+
+
+
